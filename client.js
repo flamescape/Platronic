c0e5c0239b0b326e02ab20b0fe68f3ba4d15fef9
Mikuso <g_github@flamescape.com>
2014-03-04 22:58:07 +0000

    Reorganised stuff

---
+++ client.js
@@ -0,0 +1,1 @@
+var BABYLON = require('./lib/babylon');
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var express = require('express')
   , app = express()
+  , browserify = require('browserify')
   ;
 
 app.use(express.static('public'));
+
+app.get('/client.js', function(req, res){
+    var b = browserify();
+    b.add('./client.js');
+    b.bundle(function(err, bundle){
+        if (err) {
+            console.log('Error bundling client JS', err);
+            return res.send(500, err);
+        }
+        res.send(bundle);
+    });
+});
 
 app.listen(80);
