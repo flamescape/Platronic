1a05cb48304423577cf86050ee0024e6a1fc99e5
Mikuso <g_github@flamescape.com>
2014-03-06 20:56:31 +0000

    Griddly

--- js/GridGraphicsComponent.js
+++ js/GridGraphicsComponent.js
@@ -1,7 +1,28 @@
+var BABYLON = require('../lib/babylon');
+var ServiceLocator = require('./ServiceLocator');
 
-var GridGraphicsComponent = function(){
+var GridGraphicsComponent = function(params){
+    this.plane = BABYLON.Mesh.CreatePlane(null, params.size, ServiceLocator.get('scene'), false);
+    
+    this.position = Object.create(BABYLON.Vector3.prototype);
+    this.rotation = Object.create(BABYLON.Vector3.prototype);
+    
+    BABYLON.Vector3.apply(this.position, params.position);
+    BABYLON.Vector3.apply(this.rotation, params.rotation);
+    
+    this.material = new BABYLON.StandardMaterial("grid", ServiceLocator.get('scene'));
+    this.material.diffuseTexture = new BABYLON.Texture("img/grid.svg", ServiceLocator.get('scene'));
+    this.material.diffuseTexture.hasAlpha = false;
+    this.material.backFaceCulling = true;
+    
+    // glue it all together
+    this.plane.position = this.position;
+    this.plane.rotation = this.rotation;
+    this.plane.material = this.material;
 };
 
 GridGraphicsComponent.prototype.update = function() {
     
 };
+
+module.exports = GridGraphicsComponent;
