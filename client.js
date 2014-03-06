e984036c39b54b2acd18fe917de0fd49830155cc
Kipke <bjorn.molenmaker@gmail.com>
2014-03-06 17:28:30 +0100

    Made the sphere into a cube
    and added some weird lighting effects

--- client.js
+++ client.js
@@ -9,8 +9,20 @@ scene.clearColor = new BABYLON.Color4(0,0,0,0);
 
 // Creating a camera looking to the zero point (0,0,0), a light, and a sphere of size 1
 var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
-var light0 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 10), scene);
-var origin = BABYLON.Mesh.CreateSphere("origin", 10, 1.0, scene);
+//var light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 0), scene);
+
+//var light0 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0, 1, 0), scene);
+//light0.diffuse = new BABYLON.Color3(1, 1, 1);
+//light0.specular = new BABYLON.Color3(1, 1, 1);
+//light0.groundColor = new BABYLON.Color3(0, 0, 0);
+
+var light1 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0, -1, 0), scene);
+light1.diffuse = new BABYLON.Color3(255, 0, 0);
+light1.specular = new BABYLON.Color3(0, 255,0);
+light1.groundColor = new BABYLON.Color3(0, 0, 255);
+
+// var origin = BABYLON.Mesh.CreateSphere("origin", 10, 1.0, scene);
+var box = BABYLON.Mesh.CreateBox("Box", 3.0, scene);
 
 // Attach the camera to the scene
 scene.activeCamera.attachControl(canvas);
