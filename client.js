100e50c019349b380eea1a1040b8afd5db9e86f8
John Ballantyne <johnballantyne@gmail.com>
2014-03-06 15:10:02 -0500

    4 planes

--- client.js
+++ client.js
@@ -8,7 +8,6 @@ var scene = new BABYLON.Scene(engine);
 scene.clearColor = new BABYLON.Color4(0,0,0,0);
 
 // Creating a camera looking to the zero point (0,0,0), a light, and a sphere of size 1
-var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
 //var light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 0, 0), scene);
 
 //var light0 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0, 1, 0), scene);
@@ -16,13 +15,31 @@ var camera = new BABYLON.ArcRotateCamera("Camera", 1, 0.8, 10, new BABYLON.Vecto
 //light0.specular = new BABYLON.Color3(1, 1, 1);
 //light0.groundColor = new BABYLON.Color3(0, 0, 0);
 
+/*
 var light1 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0, -1, 0), scene);
-light1.diffuse = new BABYLON.Color3(255, 0, 0);
-light1.specular = new BABYLON.Color3(0, 255,0);
-light1.groundColor = new BABYLON.Color3(0, 0, 255);
+light1.diffuse = new BABYLON.Color3(.1, 1, 1);
+light1.specular = new BABYLON.Color3(1, .1, 1);
+light1.groundColor = new BABYLON.Color3(0, 0, 1);
+*/
 
-// var origin = BABYLON.Mesh.CreateSphere("origin", 10, 1.0, scene);
-var box = BABYLON.Mesh.CreateBox("Box", 3.0, scene);
+var light0 = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(0, 0, 0), scene);
+light0.diffuse = new BABYLON.Color3(.67, .95, .98);
+light0.specular = new BABYLON.Color3(.67, .95, 98);
+
+var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 5, new BABYLON.Vector3(0, 0, 0), scene);
+light0.parent = camera;
+var p1 = BABYLON.Mesh.CreatePlane("p1", 64, scene, false);
+var p2 = BABYLON.Mesh.CreatePlane("p2", 64, scene, false);
+var p3 = BABYLON.Mesh.CreatePlane("p3", 64, scene, false);
+var p4 = BABYLON.Mesh.CreatePlane("p4", 64, scene, false);
+
+p1.position = new BABYLON.Vector3(0, 0, 32);
+p2.position = new BABYLON.Vector3(0, 0, -32);
+p3.position = new BABYLON.Vector3(32, 0, 0);
+p4.position = new BABYLON.Vector3(-32, 0, 0);
+p1.rotation = new BABYLON.Vector3(Math.PI, 0, 0);
+p3.rotation = new BABYLON.Vector3(Math.PI, Math.PI/2, 0);
+p4.rotation = new BABYLON.Vector3(0, Math.PI/2, 0);
 
 // Attach the camera to the scene
 scene.activeCamera.attachControl(canvas);
