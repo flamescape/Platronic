e06af2b75e5ae9ddb0c4afb4984f27d32c4fe79d
Kipke <bjorn.molenmaker@gmail.com>
2014-03-06 16:53:26 +0100

    Add command line args
    Designate what port to listen on using the -p <number> command line arg.
    Closes #2

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express')
   , app = express()
   , browserify = require('browserify')
-  ;
+  , program = require('commander')
+;
+
+program.option('-p, --port <n>', 'The port number', parseInt).parse(process.argv);
 
 app.use(express.static('public'));
 
-app.get('/client.js', function(req, res){
+app.get('/client.js', function (req, res) {
     var b = browserify();
     b.add('./client.js');
-    b.bundle(function(err, bundle){
+    b.bundle(function (err, bundle) {
         if (err) {
             console.log('Error bundling client JS', err);
             return res.send(500, err);
@@ -17,5 +20,7 @@ app.get('/client.js', function(req, res){
         res.send(200, bundle);
     });
 });
-
-app.listen(80);
+if (typeof program.port == 'undefined')
+    program.port = 80;
+app.listen(program.port);
+console.log('Now listening on port: ' + program.port);
