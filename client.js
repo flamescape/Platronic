5c2b934476de2134ec6a132040570f2f0e4ad7af
John Ballantyne <johnballantyne@gmail.com>
2014-03-08 12:06:01 -0500

    Move jshintrc into package.json
    And some code cleanup

--- client.js
+++ client.js
@@ -5,18 +5,18 @@ var BABYLON = require('./lib/babylon')
   , PlatonicConstructor = require('./js/PlatonicConstructor')
   ;
 
-var canvas = document.getElementById("renderCanvas");
+var canvas = document.getElementById('renderCanvas');
 var engine = new BABYLON.Engine(canvas, true);
 var scene = new BABYLON.Scene(engine);
 scene.clearColor = new BABYLON.Color4(0,0,0,0);
 ServiceLocator.provide('scene', scene);
 
-var light0 = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(0, 0, 50), scene);
+var light0 = new BABYLON.PointLight('Omni0', new BABYLON.Vector3(0, 0, 50), scene);
 light0.diffuse = new BABYLON.Color3(0.67, 0.67, 0.78);
 light0.specular = new BABYLON.Color3(0.67, 0.67, 0.78);
-light0.intensity = .5;
+light0.intensity = 0.5;
 
-var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 120, new BABYLON.Vector3(0, 0, 0), scene);
+var camera = new BABYLON.ArcRotateCamera('Camera', 0, 0, 120, new BABYLON.Vector3(0, 0, 0), scene);
 light0.parent = camera;
 
 var cube = PlatonicConstructor(64);
@@ -31,9 +31,9 @@ scene.activeCamera.attachControl(canvas);
 
 // Once the scene is loaded, just register a render loop to render it
 engine.runRenderLoop(function () {
-scene.render();
+    scene.render();
 });
 
- window.addEventListener("resize", function() {
+window.addEventListener('resize', function() {
     engine.resize();
 });
--- js/GridGraphicsComponent.js
+++ js/GridGraphicsComponent.js
@@ -1,7 +1,7 @@
 var BABYLON = require('../lib/babylon');
 var ServiceLocator = require('./ServiceLocator');
 
-var GridGraphicsComponent = function(params){
+var GridGraphicsComponent = function(params) {
     this.plane = BABYLON.Mesh.CreatePlane(null, params.size, ServiceLocator.get('scene'), false);
     
     this.position = Object.create(BABYLON.Vector3.prototype);
@@ -10,15 +10,14 @@ var GridGraphicsComponent = function(params){
     BABYLON.Vector3.apply(this.position, params.position);
     BABYLON.Vector3.apply(this.rotation, params.rotation);
     
-    this.material = new BABYLON.StandardMaterial("grid", ServiceLocator.get('scene'));
-    this.material.diffuseTexture = new BABYLON.Texture("img/grid_diffuse.png", ServiceLocator.get('scene'));
-    this.material.emissiveTexture = new BABYLON.Texture("img/grid_emiss.png", ServiceLocator.get('scene'));
+    this.material = new BABYLON.StandardMaterial('grid', ServiceLocator.get('scene'));
+    this.material.diffuseTexture = new BABYLON.Texture('img/grid_diffuse.png', ServiceLocator.get('scene'));
+    this.material.emissiveTexture = new BABYLON.Texture('img/grid_emiss.png', ServiceLocator.get('scene'));
     this.material.specularPower = 10;
-    this.material.specularTexture = new BABYLON.Texture("img/grid_spec.png", ServiceLocator.get('scene'));
+    this.material.specularTexture = new BABYLON.Texture('img/grid_spec.png', ServiceLocator.get('scene'));
     this.material.diffuseTexture.hasAlpha = false;
     this.material.backFaceCulling = true;
-    var bumpMaterial = new BABYLON.StandardMaterial("texture1", ServiceLocator.get('scene'));
-    this.material.bumpTexture = new BABYLON.Texture("img/grid_normal.png", ServiceLocator.get('scene'));
+    this.material.bumpTexture = new BABYLON.Texture('img/grid_normal.png', ServiceLocator.get('scene'));
     
     // glue it all together
     this.plane.position = this.position;
