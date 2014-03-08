81a92513827e5db676bdc71f1975e95ecfee0f24
Mikuso <g_github@flamescape.com>
2014-03-08 23:08:02 +0000

    Phys

---
+++ js/PhysicsComponent.js
@@ -0,0 +1,14 @@
+var PhysicsComponent = function(){
+};
+
+PhysicsComponent.prototype.fps = 60;
+
+PhysicsComponent.prototype.tick = function(){
+    // implemented by sub-prototypes
+};
+
+PhysicsComponent.prototype.update = function(){
+    
+};
+
+module.exports = PhysicsComponent;
