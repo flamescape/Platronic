93071f3f35c60faaf01289dc937a8f3276c44f02
John Ballantyne <johnballantyne@gmail.com>
2014-03-08 10:41:12 -0500

    Grid graphics update
    Resolves #3 and fixes #4

--- js/GridGraphicsComponent.js
+++ js/GridGraphicsComponent.js
@@ -11,14 +11,14 @@ var GridGraphicsComponent = function(params){
     BABYLON.Vector3.apply(this.rotation, params.rotation);
     
     this.material = new BABYLON.StandardMaterial("grid", ServiceLocator.get('scene'));
-    this.material.diffuseTexture = new BABYLON.Texture("img/grid.svg", ServiceLocator.get('scene'));
-    this.material.emissiveTexture = new BABYLON.Texture("img/grid_emiss.svg", ServiceLocator.get('scene'));
+    this.material.diffuseTexture = new BABYLON.Texture("img/grid_diffuse.png", ServiceLocator.get('scene'));
+    this.material.emissiveTexture = new BABYLON.Texture("img/grid_emiss.png", ServiceLocator.get('scene'));
     this.material.specularPower = 10;
-    this.material.specularTexture = new BABYLON.Texture("img/grid_spec.svg", ServiceLocator.get('scene'));
+    this.material.specularTexture = new BABYLON.Texture("img/grid_spec.png", ServiceLocator.get('scene'));
     this.material.diffuseTexture.hasAlpha = false;
     this.material.backFaceCulling = true;
     var bumpMaterial = new BABYLON.StandardMaterial("texture1", ServiceLocator.get('scene'));
-    this.material.bumpTexture = new BABYLON.Texture("img/normalMap.jpg", ServiceLocator.get('scene'));
+    this.material.bumpTexture = new BABYLON.Texture("img/grid_normal.png", ServiceLocator.get('scene'));
     
     // glue it all together
     this.plane.position = this.position;
