f4bc958e48640ce8d99351c68907d33fae5b8f79
Mikuso <g_github@flamescape.com>
2014-03-10 21:22:54 +0000

    Physics step stuff

--- js/PhysicsComponent.js
+++ js/PhysicsComponent.js
@@ -1,14 +1,23 @@
 var PhysicsComponent = function(){
+    this._lastUpdate = Date.now();
+    this._acc = 0;
 };
 
-PhysicsComponent.prototype.fps = 60;
+PhysicsComponent.prototype.fps = 1000 / 60 << 0;
 
 PhysicsComponent.prototype.tick = function(){
     // implemented by sub-prototypes
+    console.log('tick');
 };
 
 PhysicsComponent.prototype.update = function(){
-    
+    var now = Date.now();
+    this._acc += (now - this._lastUpdate);
+    this._lastUpdate = now;
+    while (this._acc > this.fps) {
+        this._acc -= this.fps;
+        this.tick();
+    }
 };
 
 module.exports = PhysicsComponent;
