e1f6ec3e95963feb7c64c973e3fdcf7409853731
Mikuso <g_github@flamescape.com>
2014-03-10 21:41:04 +0000

    turn step

--- js/BikePhysicsComponent.js
+++ js/BikePhysicsComponent.js
@@ -8,12 +8,14 @@ var BikePhysicsComponent = function(){
 };
 BikePhysicsComponent.prototype = Object.create(PhysicsComponent.prototype);
 
+BikePhysicsComponent.prototype.turnStep = Math.PI / 200;
+
 BikePhysicsComponent.prototype.tick = function(bike){
     // this is where the movement magic happens!
     bike.x += this.speed * Math.cos(this.angle);
     bike.y += this.speed * Math.sin(this.angle);
     
-    this.angle += Math.PI / 200;
+    this.angle += this.turnStep; // for test lulz
 };
 
 module.exports = BikePhysicsComponent;
